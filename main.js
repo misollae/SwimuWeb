8b35cbf48716e02fea837ead1e97a54a2657ddd4
Daniela Levezinho <daniela.levezinho@sky.uk>
2023-05-08 15:08:44 +0100

    attemp at communication with arduino

--- main.js
+++ main.js
@@ -4,15 +4,26 @@ const port = 3000;
 const http = require('http').Server(server);
 const { SerialPort } = require('serialport')
 
-server.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 const serialPort = new SerialPort({path: 'COM10', baudRate: 9600 }, function (err) {
   if (err) {
     return console.log('Error: ', err.message)
   }
 })
+
+
+server.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+function requestFile(){
+    let file_name = document.getElementById(file_name)
+    serialPort.write(file_name)
+    // server.get('/', (req, res) => {
+    //     res.send(file_name)
+    // })
+}
+
 
 serialPort.write('Begin transfer', function(err) {
   if (err) {
