2c9ef04db8d6d03ca18a59352e47f2a301cde872
misollae <leticia.isb.lucas@gmail.com>
2023-05-09 13:50:31 +0100

    Buttons and communication

--- browser/index.js
+++ browser/index.js
@@ -13,5 +13,28 @@ function requestFile(){
       body: JSON.stringify(payload)
       })
 }
-
+function showList() {
+ fetch("http://localhost:3000/SwimuWeb/FileList", {
+    method: "GET",
+    headers: {
+      Accept: "application.json",
+      "Content-Type": "application/json",
+    },
+  }).then(response => response.json())
+  .then(data => {
+    for (const num in data) {
+      const date = data[num];
+      let button = document.createElement("button");
+      button.textContent = date;
+      function handleClick() {
+        requestFile(num);
+      }
+      button.onclick = handleClick;
+      document.body.appendChild(button);
+    }
+  }
+    
+    )
+  .catch(error => console.log(error));
+}
 
